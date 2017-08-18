9f297635a2cf433eb572ff1a4f22551a1fe36e9c
Bjorn Krols <krols.bj@gmail.com>
2017-08-18 19:37:42 +0200

    Upgrade to latest version of Bootstrap (alpha -> beta)

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,6 @@
 			  <li class="breadcrumb-item active">Data</li>
 			</ol>
 		</div>
-		<!-- Badges-->
-		<h2>Badges</h2>
-		<div style="padding: 1rem;">
-		  <span v-for="brand in ['default', 'primary', 'success', 'info', 'warning', 'danger']">
-			  <span class="badge" :class="'badge-' + brand" style="margin-right: 0.333rem;">
-				  {{brand}}
-			  </span>
-		  </span>
-		</div>
 		<!-- Buttons-->
 		<h2>Buttons</h2>
 		<div style="padding: 1rem;">
@@ -54,31 +45,6 @@
 		  <div v-for="brand in ['success', 'info', 'warning', 'danger']" class="progress" style='margin-bottom: 1rem;'>
 			  <div class="progress-bar" :class="'bg-' + brand" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 		  </div>
-		</div>
-		<!-- Navbar -->
-		<h2>Navbar</h2>
-		<div style="padding: 1rem;">
-			<div v-for="color in ['bg-inverse', 'bg-primary']" style="padding-bottom: 1rem;">
-				<nav class="navbar navbar-inverse" :class="color">
-					<a class="navbar-brand" href="#">{{color}} navbar</a>
-				    <div class="collapse navbar-collapse" id="">
-				      <ul class="navbar-nav">
-				        <li class="nav-item active">
-				          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				        </li>
-				        <li class="nav-item">
-				          <a class="nav-link" href="#">Features</a>
-				        </li>
-				        <li class="nav-item">
-				          <a class="nav-link" href="#">Pricing</a>
-				        </li>
-				        <li class="nav-item">
-				          <a class="nav-link disabled" href="#">Disabled</a>
-				        </li>
-				      </ul>
-				    </div>
-				</nav>
-			</div>
 		</div>
 		<!-- Notice-->
 		<h2>Modal</h2>
